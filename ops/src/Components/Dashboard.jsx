183a2223abe75271c7af90801fbeee39910c863b
Pushpraj Patel <101568029+pushpraj15295@users.noreply.github.com>
2022-10-27 09:50:01 +0530

    Add files via upload

---
+++ ops/src/Components/Dashboard.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import EmployeeName from "./EmployeeName";
+import EmployeeSalary from "./EmployeeSalary";
+
+export const Dashboard = () => {
+
+  return (
+    <div>
+      <button
+        data-testid="sorting-btn"
+      >
+        Top 10 Employees
+      </button>
+
+      <div data-testid="employee-data">
+        {/* <EmployeeName /> */}
+        {/* <EmployeeSalary /> */}
+      </div>
+      
+    </div>
+  );
+};
